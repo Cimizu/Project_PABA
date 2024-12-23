86a225a63349a5354e8220f629dc057cbae893f0
Cimizu <gracienne24@gmail.com>
2024-12-23 18:13:59 +0700

    Login UI

---
+++ app/src/main/java/project/paba/app/Customer.kt
@@ -0,0 +1,8 @@
+package project.paba.app
+
+data class Customer(
+    val nama: String = "",
+    val email: String = "",
+    val nomorTelepon: String = "",
+    val password: String = ""
+)
--- app/src/main/java/project/paba/app/MainActivity.kt
+++ app/src/main/java/project/paba/app/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnTest = findViewById<Button>(R.id.btnTest)
+        val btnTest = findViewById<Button>(R.id.btnStart)
         btnTest.setOnClickListener {
             val intent = Intent(this, booking::class.java)
             startActivity(intent)
