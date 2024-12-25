34e8c09e5058a1d34c5d7e3c37e21b9546165a56
KUCHANAWE <137408771+KUCHANAWE@users.noreply.github.com>
2024-12-25 17:56:14 +0700

    restoran, detail restoran, profile, edit profile, paket, sign in, register

--- app/src/main/java/project/paba/app/navbar.kt
+++ app/src/main/java/project/paba/app/navbar.kt
@@ -1,20 +1,26 @@
 package project.paba.app
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class history : AppCompatActivity() {
+class navbar: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_history)
+        setContentView(R.layout.activity_navbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.frameContainer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnhome = findViewById<Button>(R.id.btnhome)
+        val btnhistory = findViewById<Button>(R.id.btnhistory)
+        val btnprofile = findViewById<Button>(R.id.btnprofile)
     }
+
 }
