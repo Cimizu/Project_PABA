20441902c76e1173e5ac03838c8eb711e4a95e8b
Cimizu <gracienne24@gmail.com>
2024-12-26 09:44:24 +0700

    Ubah tampilan paket dan resto

--- app/src/main/java/project/paba/app/dataRestoran.kt
+++ app/src/main/java/project/paba/app/dataRestoran.kt
@@ -1,7 +1,10 @@
 package project.paba.app
 
 data class dataRestoran(
-    val nama: String = "",
+    val namaResto: String = "",
     val deskripsi: String = "",
-    val alamat: String = ""
+    val alamat: String = "",
+    val foto : String = "",
+    val jamBuka :String = "",
+    val jamTutup : String = ""
 )
---
+++ app/src/main/java/project/paba/app/paketRestoran.kt
@@ -0,0 +1,10 @@
+package project.paba.app
+
+data class paketRestoran(
+    val idResto: String = "",
+    val deskripsi: String = "",
+    val kapasitas: Int ,
+    val foto : String = "",
+    val harga: Double,
+    val uangDp:Double
+)
