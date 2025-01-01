2de0dfd2706bd66ee00f2676e67dc17ea4dea19b
Cimizu <gracienne24@gmail.com>
2025-01-01 12:54:17 +0700

    Squashed commit of the following:
    commit 9febc5132276b712575be85bd7cd65370c797eef
    Author: Cimizu <gracienne24@gmail.com>
    Date:   Sat Dec 28 14:37:38 2024 +0700
    
        History belum jalan
    
    commit b7e9c04808d3e5fad5ec6aaba0b8074e2096ad6f
    Merge: 31b90b9 643316b
    Author: ciaaudi <146406439+ciaaudi@users.noreply.github.com>
    Date:   Sat Dec 28 14:22:43 2024 +0700
    
        Merge branch 'booking_new'
    
    commit 31b90b970aee6f3d20576f6b55e132025129a649
    Author: ciaaudi <146406439+ciaaudi@users.noreply.github.com>
    Date:   Fri Dec 27 22:31:49 2024 +0700
    
        history sudah di comment
    
    commit b49a2117f5c3a54fa69d4e0e1e0ac38e8bfeda88
    Author: fellllll <c14220027@john.petra.ac.id>
    Date:   Fri Dec 27 22:25:39 2024 +0700
    
        ui warna
    
    commit c05d7b513ee54a1323d1872af1c63458d7563c1f
    Author: ciaaudi <146406439+ciaaudi@users.noreply.github.com>
    Date:   Fri Dec 27 22:24:11 2024 +0700
    
        bookingList from mutable to array
    
    commit c610831107ebc11716939b44b3aa182b16be89cf
    Merge: 488ecb9 cdb19ca
    Author: fellllll <c14220027@john.petra.ac.id>
    Date:   Fri Dec 27 21:24:48 2024 +0700
    
        Merge branch 'master' of https://github.com/Cimizu/Project_PABA
    
    commit 488ecb96e2d0ec96e418b0c7a2dcfda6c0acf4c6
    Merge: f219736 8ff4417
    Author: fellllll <c14220027@john.petra.ac.id>
    Date:   Fri Dec 27 21:24:15 2024 +0700
    
        history(adapter belum connect)
    
    commit f2197366384b7adc84dd952510d3794b171bfbc5
    Author: fellllll <c14220027@john.petra.ac.id>
    Date:   Fri Dec 27 21:16:21 2024 +0700
    
        history(adapter blm connect)

--- app/src/main/java/project/paba/app/MainActivity.kt
+++ app/src/main/java/project/paba/app/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
