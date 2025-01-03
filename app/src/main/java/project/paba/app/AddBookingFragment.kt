503fd9ba71fc49de3e9dc440c251b10537b84309
Cimizu <gracienne24@gmail.com>
2025-01-03 16:06:10 +0700

    Ubah type input jadi number

--- app/src/main/java/project/paba/app/AddBookingFragment.kt
+++ app/src/main/java/project/paba/app/AddBookingFragment.kt
@@ -13,7 +13,6 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
